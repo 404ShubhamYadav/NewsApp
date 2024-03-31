176c8a953d99873b25fa9bfd04c7631db1681c75
Shubham Kr. Yadav <shubhamyadav5594@gmail.com>
2024-03-31 19:24:27 +0530

    2nd commit

--- src/components/NewssItem.js
+++ src/components/NewssItem.js
@@ -1,8 +1,7 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class NewssItem extends Component {
-    render() {
-        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
+const NewssItem = (props)=> {
+        let { title, description, imageUrl, newsUrl, author, date, source } = props;
         return (
             <div className='my-3'>
                 <div className="card">
@@ -26,6 +25,6 @@ export class NewssItem extends Component {
                 </div>
             </div >
         )
-    }
+    
 }
-export default NewssItem
+export default NewssItem;
